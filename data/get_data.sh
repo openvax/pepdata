c06486705604eb4f333a1713a8b8bc31f70b0c1b
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-03-04 20:25:10 -0500

    pass 2 simple unit tests

--- data/get_data.sh
+++
@@ -1,6 +0,0 @@
-# download T-cell response assay results
-wget -nc http://www.iedb.org/doc/tcell_compact.zip && unzip tcell_compact.zip && rm tcell_compact.zip
-
-# download MHC binding assay results 
-wget -nc http://www.iedb.org/doc/elution_compact.zip && unzip elution_compact.zip && rm elution_compact.zip
-
--- test/test_iedb.py
+++ test/test_iedb.py
@@ -1,15 +1,30 @@
-import iedb 
+from epitope import iedb
 
-def test_hla_a24():
+def test_tcell_hla_a24():
   """
-  Test that HLA restriction actually decreases number of results and 
+  Test that HLA restriction actually decreases number of results and
   that regular expression patterns are being used correctly
   """
-  df_all = iedb.load_tcell_prct()
-  df_a24_1 = iedb.load_tcell_prct(hla_type='HLA-A24')
-  df_a24_2 = iedb.load_tcell_prct(hla_type='HLA-A\*24')
-  df_a24_combined = iedb.load_tcell_prct(hla_type = 'HLA-A24|HLA-A\*24')
+  df_all = iedb.load_tcell()
+  df_a24_1 = iedb.load_tcell(hla_type='HLA-A24')
+  df_a24_2 = iedb.load_tcell(hla_type='HLA-A\*24')
+  df_a24_combined = iedb.load_tcell(hla_type = 'HLA-A24|HLA-A\*24')
   assert len(df_a24_1) < len(df_all)
   assert len(df_a24_2) < len(df_all)
-  assert len(df_combined) == len(df_a24_1) + len(df_a24_2)
+  assert len(df_a24_combined) <= len(df_a24_1) + len(df_a24_2), \
+   "Expected %d <= %d + %d" % (len(df_a24_combined), len(df_a24_1), len(df_a24_2))
 
+
+def test_mhc_hla_a2():
+  """
+  Test that HLA restriction actually decreases number of results and
+  that regular expression patterns are being used correctly
+  """
+  df_all = iedb.load_mhc()
+  df_a2_1 = iedb.load_mhc(hla_type='HLA-A2')
+  df_a2_2 = iedb.load_mhc(hla_type='HLA-A\*02')
+  df_a2_combined = iedb.load_mhc(hla_type = 'HLA-A2|HLA-A\*02')
+  assert len(df_a2_1) < len(df_all)
+  assert len(df_a2_2) < len(df_all)
+  assert len(df_a2_combined) <= len(df_a2_1) + len(df_a2_2), \
+    "Expected %d <= %d + %d" % (len(df_a2_combined), len(df_a2_1), len(df_a2_2))
