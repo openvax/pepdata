872abdd2e08666deb5cebfca0900611c408e7de5
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2014-04-04 19:05:29 -0400

    changed mutate functions to return struct with positions and annotation

--- epitopes/mutate.py
+++ epitopes/mutate.py
@@ -74,23 +74,23 @@ def is_frameshift(variant_length):
     return variant_length > 1 and variant_length % 3 != 0
 
 MutationRegion = \
-    namedtuple("MutationRegion",
-    (
-        "seq",   # amino acid sequence of region around mutation
-        "start"  # where in the original protein did we start?
-        "stop"   # where in the original protein did we end?
-        "mutation_start", # where in the region does the mutation start?
-        "mutation_stop",  # where in the region does the mutation end?
-        "frameshift" # was the mutation a frameshift?
-    )
+    namedtuple(
+        "MutationRegion",
+        (
+            "seq",   # amino acid sequence of region around mutation
+            "start",  # where in the original protein did we start?
+            "stop",   # where in the original protein did we end?
+            "mutation_start", # where in the region does the mutation start?
+            "mutation_stop",  # where in the region does the mutation end?
+            "frameshift" # was the mutation a frameshift?
+        ))
 
 def get_mutation_region(
         seq,
         position,
         variant_length = 1,
         max_length = None,
-        min_padding = None,
-        with_mutation_coordinates = False):
+        min_padding = None):
     """
     Get surrounding region of a sequence around a specific position.
 
@@ -111,9 +111,11 @@ def get_mutation_region(
     min_padding : int, optional
         Minimum amount of residues before and after variant affected residues
 
-    return_info : bool (default = False)
-        Return start/stop for region, start/stop of mutation in region, and
-        boolean flag for frameshifts
+    Returns MutationRegion struct with:
+     - 'seq': mutated sequence
+     - 'start'/'stop': start/stop of region in the context sequence
+     - 'mutation_start'/'mutation_stop': start/stop of mutation in the region
+     - 'frameshift' : was the mutation a frameshift?
     """
     n = len(seq)
     if max_length is None:
@@ -138,33 +140,80 @@ def get_mutation_region(
         frameshift = False
 
     end_codon = str(seq[position:]).find("*")
-    if end_codon > 0:
+    if end_codon >= 0:
         end_pos = min(end_pos, position + end_codon)
 
     seq_region = seq[start_pos : end_pos]
+    mutation_start_pos = position - start_pos
 
     mutation_end_pos = \
         min(len(seq_region), mutation_start_pos + num_residue_affected)
-    mutation_start_pos = position - start_pos
+
     return MutationRegion(
-        seq_region, start_pos, end_pos,
+        seq_region,
+        start_pos, end_pos,
         mutation_start_pos, mutation_end_pos,
         frameshift)
 
 
 
-Mutation = namedtuple("Mutation",
-    (
-        "seq",  # mutated sequence
-        "original",  # original sequence
-        "pos",
+def _annotate(
+        original_protein, mutated_protein, region,  dna_ref, dna_alt, dna_pos):
+    aa_position = dna_pos / 3
+    ref_start = region.start + region.mutation_start
+    # deletions have same start/stop but still need to show
+    # which residue got deleted
+    ref_stop = max(ref_start+1, region.start + region.mutation_stop)
+
+    aa_ref = original_protein[ref_start : ref_stop]
+
+
+    n_ref = len(dna_ref)
+    n_alt = len(dna_alt)
+
+    # in case we hit an early stop codon
+
+    last_mutation_pos = region.start + region.mutation_stop + 1
+
+
+
+    if region.frameshift:
+        return "%s%dfs" % (aa_ref[0], aa_position+1)
+    elif n_alt == 0:
+        return "%s%ddel" % (aa_ref, aa_position+1)
+    elif n_ref == n_alt:
+        mutated_stop = min(ref_stop, last_mutation_pos)
+        return "%s%d%s" % \
+            (aa_ref, aa_position+1, mutated_protein[ref_start:mutated_stop])
+    elif n_ref > n_alt:
+        diff = (n_ref - n_alt) / 3
+        mutated_stop = min(ref_stop - diff, last_mutation_pos)
+        return "%s%d%s" % \
+            (aa_ref, aa_position+1, mutated_protein[ref_start:mutated_stop])
+    elif n_ref < n_alt:
+        diff = (n_alt - n_ref) / 3
+        mutated_stop = min(ref_stop + diff, last_mutation_pos)
+        return "%s%d%s" % \
+            (aa_ref, aa_position+1, mutated_protein[ref_start:mutated_stop])
+
+Mutation = \
+    namedtuple(
+        "Mutation",
+        (
+            "seq",    # mutated region sequence
+            "start",  # start position of region in the protein
+            "stop",   # stop position of region in the protein
+            "mutation_start", # where in the region is the first mutated AA?
+            "mutation_stop",  # where does the region go back to wildtype?
+            "annot", # mutation annotation i.e. "V600E"
+        ))
 
 
 
 def mutate_protein_from_transcript(
         transcript_seq, position, ref, alt,
-            min_padding = None,
-            max_length = None):
+        min_padding = None,
+        max_length = None):
     """
     Mutate a sequence by inserting the allele into the genomic transcript
     and translate to protein sequence
@@ -194,19 +243,31 @@ def mutate_protein_from_transcript(
             (transcript_ref_base, position, ref)
 
     mutated_dna = mutate(transcript_seq, position, ref, alt)
-    mutated_peptide = mutated_dna.translate()
+
+    mutated_protein = mutated_dna.translate()
 
     aa_position = int(position / 3)  # genomic position to codon position
+
     variant_length = max(len(ref), len(alt))
     if max_length:
         variant_length = min(variant_length, max_length)
-   region = \
+
+    region = \
         get_mutation_region(
-            mutated_peptide,
+            mutated_protein,
             aa_position,
             variant_length,
-            min_padding = min_padding,
-            with_mutation_coordinates = True)
-    original_peptide = transcript_seq.translate()_
-    aa_ref =
-    return Mutat
+            min_padding = min_padding)
+    print region
+    original_protein = transcript_seq.translate()
+    annot = \
+        _annotate(original_protein, mutated_protein, region,
+            ref, alt, position)
+
+    return Mutation(
+        seq = str(region.seq),
+        start = region.start,
+        stop = region.stop,
+        mutation_start = region.mutation_start,
+        mutation_stop = region.mutation_stop,
+        annot = annot)
