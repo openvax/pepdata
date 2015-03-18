15c5d7419c92e5254614ee288ad2a12b0e218e51
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2015-03-18 16:13:49 -0400

    increase version, incompatible API change to IEDB code

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from setuptools import setup
 if __name__ == '__main__':
     setup(
         name='pepdata',
-        version="0.5.0",
+        version="0.6.0",
         description="Python interface to IEDB and other immune epitope data",
         author="Alex Rubinsteyn",
         author_email="alex {dot} rubinsteyn {at} mssm {dot} edu",
