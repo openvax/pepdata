7ef4cf2def90c7b292809a1c878a0c63094dfc88
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2018-02-21 16:23:38 -0500

    removed keras & theano reqs from travis

--- pepdata/__init__.py
+++ pepdata/__init__.py
@@ -10,7 +10,7 @@ from .amino_acid_alphabet import (
 from .peptide_vectorizer import PeptideVectorizer
 from . import iedb
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 __all__ = [
     "iedb",
