64c072d7ba6849f4e36010e8cac3713a2f1f367a
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2026-04-08 09:48:01 -0400

    Replace nose.tools.eq_ with assert in tests

--- tests/test_amino_acids.py
+++ tests/test_amino_acids.py
@@ -1,4 +1,3 @@
-from nose.tools import eq_
 from pepdata.amino_acid_alphabet import (
     canonical_amino_acids,
     canonical_amino_acid_letters,
@@ -13,7 +12,7 @@ def test_canonical_amino_acids_letters():
     assert len(canonical_amino_acid_letters) == 20
     assert "X" not in canonical_amino_acid_letters
     expected_letters = [aa.letter for aa in canonical_amino_acids]
-    eq_(expected_letters, canonical_amino_acid_letters)
+    assert expected_letters == canonical_amino_acid_letters
 
 def test_extended_amino_acids():
     assert len(extended_amino_acids) > 20
@@ -23,4 +22,4 @@ def test_extended_amino_acids_letters():
     assert "X" in extended_amino_acid_letters
     assert "J" in extended_amino_acid_letters
     expected_letters = [aa.letter for aa in extended_amino_acids]
-    eq_(expected_letters, extended_amino_acid_letters)
+    assert expected_letters == extended_amino_acid_letters
